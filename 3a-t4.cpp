17b507d22b093da07de7e1cc119e2c50f484e949
Kate <163582212+Genopof@users.noreply.github.com>
2024-10-27 17:38:29 +0300

    Create 3a-t4.cpp

---
+++ 3a-t4.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+cout << "Enter Ehe anount of Integers to sum = "; cin >> k;
+do {
+cout << "Enter integer nr." << i << ":";
+cin ›> number;
+sum_k += number; i++;
+} while (i <= k);
+cout << "The total sum of " << k <<" integers is: " « sum_k;
+return 0;
+}
